fd28a9c6abad310329665afaa2203837500b54ef
vprathap21 <211501064@rajalakshmi.edu.in>
2024-05-23 18:22:11 +0530

    constants.js updated

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,5 +1,5 @@
 
-export const MENU_IMAGE = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+export const MENU_IMAGE = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 export const CDN_URL ="https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 export const MENU_URL =
 "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
